2fa76dd3a280ca2378fb4eeaa0bd7dc2daec66f8
amannagle <aman.nagle1999@gmail.com>
2023-08-16 00:53:30 +0530

    added config for deployment

--- model/messages.js
+++ model/messages.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const url = `mongodb+srv://amanagent98:aman1234@cluster0.vmwqufu.mongodb.net/Messageboard?retryWrites=true&w=majority`;
-
+const url = process.env.mongodbURL
+console.log("url is ",url)
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
-
 const messageSchema = new mongoose.Schema({
   user:String,
   text:String,
